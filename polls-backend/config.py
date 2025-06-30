961d96ebfbc0272110331aa1c653bd93c5c6795a
Nicholas Wachira <wachiramwas122@gmail.com>
2025-06-30 19:58:08 +0300

    Clean commit after reverting merge changes

--- polls-backend/config.py
+++ polls-backend/config.py
@@ -1,6 +1,9 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///polls.db")
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "postgresql://polls_db_csp7_user:oMNUwfm95Ki6Gh9YRJ472ExcyD7JUA2c@dpg-d1hbv9qli9vc73bjm6sg-a/polls_db_csp7"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
